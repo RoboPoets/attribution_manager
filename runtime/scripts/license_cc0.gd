88cf12b31415692f643578cad71903bb08baf8de
Andreas Raddau <and.rad@posteo.de>
2024-04-11 15:04:25 +0200

    Added some default license types

---
+++ runtime/scripts/license_cc0.gd
@@ -0,0 +1,7 @@
+@tool
+class_name LicenseCC0 extends LicenseBase
+
+
+func _init():
+	license = "CC0"
+	license_url = "https://creativecommons.org/publicdomain/zero/1.0/"
---
+++ runtime/scripts/license_cc_by.gd
@@ -0,0 +1,7 @@
+@tool
+class_name LicenseCCBY extends LicenseBase
+
+
+func _init():
+	license = "CC-BY"
+	license_url = "https://creativecommons.org/licenses/by/4.0/"
