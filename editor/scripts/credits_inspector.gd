88e2ea4c305b1e31e7178cdffc88b6bc5cdd34ed
Andreas Raddau <raddau@robopoets.com>
2024-04-15 09:48:04 +0200

    Improved Inspector UI clean-up between selecting different resources

--- editor/scripts/credits_inspector.gd
+++ editor/scripts/credits_inspector.gd
@@ -1,15 +1,15 @@
 extends EditorInspectorPlugin
 
 
-var credits_info:Control = null
-
-
 func _can_handle(object:Object) -> bool:
 	return object is Resource
 
 
 func _parse_end(object:Object) -> void:
-	if not credits_info:
-		credits_info = preload("../ui/credits_info.tscn").instantiate()
+	# In past versions, we stored a reference to this in order to reuse the
+	# node, but it seems that the inspector automatically frees it
+	# when selecting a new object, so the reference would never persist
+	# anyway.
+	var credits_info:Control = preload("../ui/credits_info.tscn").instantiate()
 	credits_info.refresh_info(object)
 	add_custom_control(credits_info)
--- editor/ui/credits_info.gd
+++ editor/ui/credits_info.gd
@@ -5,16 +5,25 @@ extends Control
 var _license_classes:Array[Dictionary] = []
 
 
-func _enter_tree()-> void:
+# We use this to guard against cases where the scene for this UI element is
+# open and we're using its functionality on resources at the same time. This
+# would cause changes to the options button to persist, which is not at all
+# what we'd want.
+func _notification(what:int) -> void:
+	match what:
+		NOTIFICATION_EDITOR_PRE_SAVE:
+			%ClassListButton.clear()
+
+
+func refresh_info(object:Object) -> void:
+	%ClassListButton.add_item("None")
+	%ClassListButton.selected = 0
+
 	for c in ProjectSettings.get_global_class_list():
 		if c.base != "LicenseBase" and c.class != "LicenseBase":
 			continue
 		_license_classes.append(c)
 		%ClassListButton.add_item(c.class)
-
-
-func refresh_info(object:Object) -> void:
-	print("duuuude")
 
 
 func _on_attribution_class_selected(index:int) -> void:
@@ -26,6 +35,8 @@ func _on_attribution_class_selected(index:int) -> void:
 
 
 func _build_license_inspector(class_path:String) -> void:
+	_clear_license_inspector()
+
 	var license:LicenseBase = load(class_path).new()
 	for prop in license.get_property_list():
 		if prop.usage != 4102:
@@ -39,3 +50,11 @@ func _build_license_inspector(class_path:String) -> void:
 		hbox.add_child(label)
 		hbox.add_child(edit)
 		$Properties.add_child(hbox)
+
+
+func _clear_license_inspector() -> void:
+	var count := $Properties.get_child_count()
+	for i in range(count - 1, -1, -1):
+		var node := $Properties.get_child(i)
+		$Properties.remove_child(node)
+		node.queue_free()
