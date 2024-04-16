c17827eb6d20e817b21d57fc194f7e00abf78a77
Andreas Raddau <and.rad@posteo.de>
2024-04-16 14:18:38 +0200

    Multiple licenses can be attached to a single resource

--- editor/ui/credits_info.gd
+++ editor/ui/credits_info.gd
@@ -2,61 +2,22 @@
 extends Control
 
 
+@export var license_info:PackedScene = preload("uid://cs5uset63g8ea")
+
 var _license_classes:Array[Dictionary] = []
-
-
-# We use this to guard against cases where the scene for this UI element is
-# open and we're using its functionality on resources at the same time. This
-# would cause changes to the options button to persist, which is not at all
-# what we'd want.
-func _notification(what:int) -> void:
-	match what:
-		NOTIFICATION_EDITOR_PRE_SAVE:
-			%ClassListButton.clear()
 
 
 func refresh_info(object:Object) -> void:
 	$AddButton.icon = get_theme_icon("Add", "EditorIcons")
 	$CategoryPanel/HBoxContainer/Icon.modulate = get_theme_color("font_color", "Label")
-	%ClassListButton.add_item("None")
-	%ClassListButton.selected = 0
 
 	for c in ProjectSettings.get_global_class_list():
-		if c.base != "LicenseBase" and c.class != "LicenseBase":
+		if c.base != "LicenseBase":
 			continue
 		_license_classes.append(c)
-		%ClassListButton.add_item(c.class)
 
 
-func _on_attribution_class_selected(index:int) -> void:
-	var selection = %ClassListButton.get_item_text(index)
-	for c in _license_classes:
-		if c.class == selection:
-			_build_license_inspector(c.path)
-			break
-
-
-func _build_license_inspector(class_path:String) -> void:
-	_clear_license_inspector()
-
-	var license:LicenseBase = load(class_path).new()
-	for prop in license.get_property_list():
-		if prop.usage != 4102:
-			continue
-		var label := Label.new()
-		label.text = prop.name.capitalize()
-		label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		var edit := TextEdit.new()
-		edit.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		var hbox := HBoxContainer.new()
-		hbox.add_child(label)
-		hbox.add_child(edit)
-		$Properties.add_child(hbox)
-
-
-func _clear_license_inspector() -> void:
-	var count := $Properties.get_child_count()
-	for i in range(count - 1, -1, -1):
-		var node := $Properties.get_child(i)
-		$Properties.remove_child(node)
-		node.queue_free()
+func _on_add_button_pressed():
+	var info := license_info.instantiate()
+	info.set_licenses(_license_classes)
+	$Licenses.add_child(info)
---
+++ editor/ui/license_info.gd
@@ -0,0 +1,44 @@
+@tool
+extends Control
+
+
+var _licenses:Array[Dictionary] = []
+
+
+func _on_attribution_class_selected(index:int) -> void:
+	_clear_license_inspector()
+	var selection = %ClassListButton.get_item_text(index)
+	for c in _licenses:
+		if c.class == selection:
+			_build_license_inspector(c.path)
+			break
+
+
+func set_licenses(licenses:Array[Dictionary]) -> void:
+	_licenses = licenses
+	for c in _licenses:
+		%ClassListButton.add_item(c.class)
+
+
+func _build_license_inspector(class_path:String) -> void:
+	var license:LicenseBase = load(class_path).new()
+	for prop in license.get_property_list():
+		if prop.usage != 4102:
+			continue
+		var label := Label.new()
+		label.text = prop.name.capitalize()
+		label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		var edit := TextEdit.new()
+		edit.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		var hbox := HBoxContainer.new()
+		hbox.add_child(label)
+		hbox.add_child(edit)
+		$Properties.add_child(hbox)
+
+
+func _clear_license_inspector() -> void:
+	var count := $Properties.get_child_count()
+	for i in range(count - 1, -1, -1):
+		var node := $Properties.get_child(i)
+		$Properties.remove_child(node)
+		node.queue_free()
